d4e341cb9964e799bc97903c636fd7fabae1b72f
Kasra <kasrazahir@gmail.com>
2018-01-31 13:46:36 -0500

    processed

---
+++ process.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import json
+
+data = pd.read_csv("./assets/data.csv", header=None, names=['date','item', 'debit', 'credit','subCategory'],index_col=False)
+maps = pd.read_csv("./rules/1to1maps.csv", header=None, names=['item', 'subCategory'])
+subCategories = pd.read_csv("./rules/categories.csv", header=None, names=['item', 'subCategory'])
+categories = pd.read_csv("./rules/breakdown.csv", header=None, names=['subCategory', 'category'])
+
+categoryMap ={}
+for i, row in categories.iterrows():
+    categoryMap[row['subCategory']] = row['category']
+
+data.fillna("", inplace=True)
+
+subCatArray = []
+# first mapping the 1to1 mappings
+for i, row in data.iterrows():
+    if (row['subCategory'] != ""):
+        subCatArray.append(row['subCategory'])
+    else:
+        try:
+            index = pd.Index(maps['item']).get_loc(row['item'].rstrip())
+            subCategory = maps.loc[index]['subCategory']
+            subCatArray.append(subCategory)
+        except:
+            subCatArray.append("")
+            
+# then mapping all the general categories
+data['subCategory'] = subCatArray
+subCatArray = pd.Series(subCatArray)   
+
+for i, categoryRow in subCategories.iloc[::-1].iterrows():
+    indo = ((data['item'].str.contains(categoryRow['item'])) & (data['subCategory']==""))
+    subCatArray[indo] = categoryRow['subCategory']
+    
+data['balance']=data['credit']-data['debit']
+
+# finally taking care of special categories with logic
+specialCategories = subCategories[subCategories['item'].str.contains('{{')]
+for i, categoryRow in specialCategories.iterrows():
+    itemValuePair = categoryRow['item'].replace('}}', '').split('{{')
+    indo = (data['item'].str.contains(itemValuePair[0].rstrip()) & (data['balance']==(float(itemValuePair[1]))))
+    subCatArray[indo] = categoryRow['subCategory']
+
+data['subCategory'] = subCatArray
+data['category'] = data['subCategory'].map(categoryMap)
+data['year']= pd.to_datetime(data['date']).dt.year
+data['month']= pd.to_datetime(data['date']).dt.month
+
+dataWithCategory = (data[data['subCategory'] != ""])
+dataWithoutCategory = (data[data['subCategory'] == ""])
+dataWithCategory[['item','category','subCategory','date','year','month','debit','credit','balance']].sort_values(by='date', ascending=False).to_csv('./processed/processed.csv', index=False)
+dataWithoutCategory[['item','date','balance']].to_csv('./processed/not_found.csv')
+
+print('unCategorized items')
+print(dataWithoutCategory[['item','date','balance']])
