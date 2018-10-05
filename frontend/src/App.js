e813b5a94cf7097918718769a201185fdd5caf8f
Kasra <kasrazahir@gmail.com>
2018-10-05 02:14:07 -0400

    trigger test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -195,7 +195,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          hello22
+          hello21
         </header>
 
         {!this.state.processFinished &&
