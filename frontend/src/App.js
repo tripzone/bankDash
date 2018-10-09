47f5a840314cf967468c8ee5d2a87d16870f5ef4
Kasra <kasrazahir@gmail.com>
2018-10-09 16:32:07 -0400

    build trigger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -196,7 +196,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          !!!!!  Kubectl !!!!!
         </header>
 
         {!this.state.processFinished &&
