d0e713b81e080e2ea0aaaf6297166f323719d453
Kasra <kasrazahir@gmail.com>
2018-10-09 00:08:30 -0400

    Kubernetes working

--- backend/server.py
+++ backend/server.py
@@ -14,7 +14,7 @@ CORS(app)
 
 @app.route("/test")
 def home():
-	return json.dumps({"success":True, "test":"build2"}), 200, {"ContentType":"application/json"}
+	return json.dumps({"success":True, "test":"build4"}), 200, {"ContentType":"application/json"}
 
 # os.system('export GOOGLE_APPLICATION_CREDENTIALS=./private/bankdash22-storage.json')
 # print('amo',os.system('$GOOGLE_APPLICATION_CREDENTIALS'))
