ef7c5ee34838c1b3f4d184eb03d94f7bfc621927
Kasra <kasrazahir@gmail.com>
2018-10-09 16:08:26 -0400

    build triggeR

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -196,7 +196,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          !! Kubectl
+          !!!!  Kubectl
         </header>
 
         {!this.state.processFinished &&
